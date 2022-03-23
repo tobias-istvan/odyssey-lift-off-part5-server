078c1ad24175cb44c8f36b5b655ad4d40d9dc4a6
István Tóbiás <istvan.tobias@weareroam.com>
2022-03-24 00:53:26 +1300

    add new field and deprecated directive

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,11 +40,15 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+    durationInSeconds: ({ length }) => length,
 
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
   },
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  }
 };
 
 module.exports = resolvers;
